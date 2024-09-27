20aceca5743241edcd7a925e135aef1bc2522437
darkhan1236 <dzejnulla194@gmail.com>
2024-09-27 23:53:25 +0500

    new

--- Task1.py
+++ Task1.py
@@ -1,2 +1,11 @@
-n = int(input())
-n1 = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+ 
+if c < a:
+    a, c = c, a
+if b < a:
+    a, b = b, a
+if c < b:
+    b, c = c, b
+print(b)
