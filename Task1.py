e9189753dabdeecbe929d5c11c7d700c0b821171
darkhan1236 <dzejnulla194@gmail.com>
2024-09-30 09:52:00 +0500

    task1 change

--- Task1.py
+++ Task1.py
@@ -1,11 +1,11 @@
-a = int(input())
-b = int(input())
-c = int(input())
- 
-if c < a:
-    a, c = c, a
-if b < a:
-    a, b = b, a
-if c < b:
-    b, c = c, b
-print(b)
+a = []
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in range(0, 8):
+    temp_a = []
+    for j in range(0, 8):
+        temp_a.append(x[j])
+        x[j] = x[j] * x[j]
+    
+    a.append(temp_a)    
+print(a)    
